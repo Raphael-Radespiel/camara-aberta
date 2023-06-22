3da770d49638043e5c183c6651e085a49b6cf3df
Raphael Radespiel <contatoraphael1701@gmail.com>
2023-06-22 09:24:14 -0300

    Create NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 function App() {
+
   return (
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    <>
+      <nav className="flex justify-between items-center 
+                       px-24
+                       bg-black text-white 
+                       text-lg font-bold font-roboto">
+        <a href="/" className="flex items-center gap-1
+                                border-2 border-white 
+                                my-4 p-1">
+          <div className="">Câmara</div>
+          <div className="bg-cyan text-black px-2 py-1">Aberta</div>
+        </a>
+        <ul className="flex gap-12 items-center">
+          <li><a href="/search/deputados">Deputados</a></li>
+          <li><a href="/search/partidos">Partidos</a></li>
+          <li><a href="/search/proposições">Proposições</a></li>
+          <li><a href="/search/eventos">Eventos</a></li>
+        </ul>
+        <a href="/sobre" className="border-b-4 border-yellow">Sobre o Projeto</a>
+      </nav>
+      <main>
+        <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      </main>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        cyan: "#5AC7C7",
+        yellow: "#FFFF42",
+      },
+      fontFamily:{
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
