928930cbede90c054e3fdbdbf6009dd64620b064
Raphael Radespiel <contatoraphael1701@gmail.com>
2023-07-06 15:48:35 -0300

    Display deputado json data

--- client/pages/search/DeputadosSearch.jsx
+++ client/pages/search/DeputadosSearch.jsx
@@ -4,10 +4,17 @@ import axios from "axios"
 const baseURL = "http://localhost:5000";
 
 export default function DeputadosSearch() {
+
+  const [deputados, setDeputados] = useState([]);
+
+  // PESQUISAR POR NOME E LEGISLATURA
   
   useEffect(() => {
     axios.get(`${baseURL}/deputados`)
-      .then(res => console.log(res.data))
+      .then(res => {
+        console.log(res.data.dados);
+        setDeputados(res.data.dados);
+      })
       .catch(err => console.log(err));
   }, []);
 
@@ -18,7 +25,18 @@ export default function DeputadosSearch() {
     <>
       {/*SEARCH COMPONENTS AREA*/}
       <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
-      <p>what</p>
+      {
+        deputados.map((value, index) => {
+          return (
+            <div key={index}>
+              <h1>{value.nome}</h1>
+              <p>email: {value.email}</p>
+              <p>partido: {value.siglaPartido}</p>
+              <img src={value.urlFoto} className="w-20"></img>
+            </div>
+          )
+        })
+      }
     </>
 
   )
